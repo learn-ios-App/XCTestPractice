b571112c46d681ca359211ebba16001a60de7681
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2024-02-13 23:04:44 +0900

    commit

---
+++ XCTestPractice/Calclater.swift
@@ -0,0 +1,20 @@
+//
+//  Calclater.swift
+//  XCTestPractice
+//
+//  Created by 渡邊魁優 on 2024/02/13.
+//
+
+import Foundation
+
+class Calclater {
+    func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    func mul(a: Int, b: Int) -> Int {
+        return a * b
+    }
+}
--- XCTestPracticeTests/XCTestPracticeTests.swift
+++ XCTestPracticeTests/XCTestPracticeTests.swift
@@ -8,29 +8,27 @@
 import XCTest
 @testable import XCTestPractice
 
-final class XCTestPracticeTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+class XCTestPracticeTests: XCTestCase {
+    var calclater: Calclater!
+    
+    override func setUp() {
+        calclater = Calclater()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func tearDown() {
+        calclater = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testAdd() {
+        XCTAssertEqual(calclater.add(a: 1, b: 1), 2)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testSub() {
+        XCTAssertEqual(calclater.sub(a: 1, b: 1), 0)
     }
-
+    
+    func testMul() {
+        XCTAssertEqual(calclater.mul(a: 1, b: 1), 1)
+    }
 }
+
