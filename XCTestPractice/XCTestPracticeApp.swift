828a3357f7dafacb21d5f36f5a85390215572d3a
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2024-02-13 22:41:38 +0900

    Initial Commit

---
+++ XCTestPractice/XCTestPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCTestPracticeApp.swift
+//  XCTestPractice
+//
+//  Created by 渡邊魁優 on 2024/02/13.
+//
+
+import SwiftUI
+
+@main
+struct XCTestPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
